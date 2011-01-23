21c7c276c116fef3e12e1cb7cd725999c5edccd0
Robert <xperimental@solidproject.de>
2011-01-24 00:10:46 +0100

    Display correct time in device.

--- src/net/sourcewalker/olv/messages/calls/GetTimeResponse.java
+++ src/net/sourcewalker/olv/messages/calls/GetTimeResponse.java
@@ -1,14 +1,24 @@
 package net.sourcewalker.olv.messages.calls;
 
 import java.nio.ByteBuffer;
+import java.util.Date;
 
 import net.sourcewalker.olv.messages.LiveViewCall;
 import net.sourcewalker.olv.messages.MessageConstants;
 
 public class GetTimeResponse extends LiveViewCall {
 
-    public GetTimeResponse() {
+    private int time;
+
+    public GetTimeResponse(Date time) {
         super(MessageConstants.MSG_GETTIME_RESP);
+
+        this.time = (int) (time.getTime() / 1000);
+        this.time -= time.getTimezoneOffset() * 60;
+    }
+
+    public GetTimeResponse() {
+        this(new Date());
     }
 
     /*
@@ -18,7 +28,7 @@ public class GetTimeResponse extends LiveViewCall {
     @Override
     protected byte[] getPayload() {
         ByteBuffer buffer = ByteBuffer.allocate(5);
-        buffer.putInt((int) (System.currentTimeMillis() / 1000));
+        buffer.putInt(time);
         buffer.put((byte) 0);
         return buffer.array();
     }
