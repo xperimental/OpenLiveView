ccfea6f4ea103e1157305c008d99d592be726128
Robert <xperimental@solidproject.de>
2011-02-01 23:54:39 +0100

    Clearing log now possible.

--- src/net/sourcewalker/olv/LogViewActivity.java
+++ src/net/sourcewalker/olv/LogViewActivity.java
@@ -3,6 +3,8 @@ package net.sourcewalker.olv;
 import net.sourcewalker.olv.data.LiveViewData;
 import android.app.ListActivity;
 import android.database.Cursor;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.SimpleCursorAdapter;
 
 /**
@@ -28,6 +30,40 @@ public class LogViewActivity extends ListActivity {
                         LiveViewData.Log.DATETIME, LiveViewData.Log.MESSAGE },
                 new int[] { android.R.id.text1, android.R.id.text2 });
         setListAdapter(adapter);
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see android.app.Activity#onCreateOptionsMenu(android.view.Menu)
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.log_menu, menu);
+        return true;
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see android.app.Activity#onOptionsItemSelected(android.view.MenuItem)
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case R.id.menu_clearlog:
+            clearLog();
+            break;
+        default:
+            throw new IllegalArgumentException("Unknown menu item: "
+                    + item.getItemId());
+        }
+        return true;
+    }
+
+    /**
+     * Clears the current contents of the application log.
+     */
+    private void clearLog() {
+        getContentResolver().delete(LiveViewData.Log.CONTENT_URI, null, null);
     }
 
 }
